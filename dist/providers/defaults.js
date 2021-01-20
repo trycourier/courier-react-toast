8872d59a166634a59586940d801d8aeb7fc42878
RoyalSix <jay.a.scottjr@biola.edu>
2021-01-19 17:13:37 -0800

    updated build

--- dist/providers/defaults.js
+++ dist/providers/defaults.js
@@ -20,7 +20,7 @@ var defaultConfig = {
       boxShadow: "rgba(157, 55, 137, 0.2) 0px 4px 12px",
       borderRadius: 5,
       color: "black",
-      padding: 10
+      margin: 10
     },
     title: {
       fontWeight: "bold",
