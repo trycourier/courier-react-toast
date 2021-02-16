2dadad9c16855fba89e185315fdee05040dbd332
RoyalSix <jay.a.scottjr@biola.edu>
2021-02-16 11:22:33 -0800

    added subscribe/unsubscribe functionality

--- stories/Toast With Transport.stories.js
+++ stories/Toast With Transport.stories.js
@@ -5,29 +5,13 @@ import React, {
 import {
   Toast, ToastProvider, CourierTransport,
 } from "../src";
-import { Button } from "./styled";
+import {
+  Button, Input, Label,
+} from "./styled";
 
 export default {
   title: "Example/Toast",
   component: Toast,
-  argTypes:{
-    channel: {
-      control: {
-        type:"select",
-        options:["template", "notification"],
-      },
-    },
-    event: {
-      control: {
-        type:"select",
-        options: ["published", "sent"],
-      },
-    },
-  },
-  args: {
-    channel: "template",
-    event: "published",
-  },
 };
 
 export function WithCourierTransport({ event, channel }) {
@@ -38,9 +22,8 @@ export function WithCourierTransport({ event, channel }) {
       apiKey: "test-api-key",
       clientKey: "test-client-key",
     });
-    courierTransport.subscribe(channel, event);
     setTransport(courierTransport);
-  }, [channel, event]);
+  }, []);
 
   const createTestEvent = useCallback(() => {
     transport.send({
@@ -55,13 +38,60 @@ export function WithCourierTransport({ event, channel }) {
       },
     });
   }, [channel, event, transport]);
+  const unsubscribe = useCallback((channel, event) => {
+    transport.unsubscribe(channel, event);
+  }, [transport]);
+  const subscribe = useCallback((channel, event) => {
+    transport.subscribe(channel, event);
+  }, [transport]);
   return (
     <ToastProvider transport={transport}>
-      <CourierTransportExample createTestEvent={createTestEvent} />
+      <CourierTransportExample
+        subscribe={subscribe}
+        unsubscribe={unsubscribe}
+        channel={channel}
+        event={event}
+        createTestEvent={createTestEvent} />
     </ToastProvider>
   );
 }
 
-function CourierTransportExample({ createTestEvent }) {
-  return <Button onClick={createTestEvent}>Send Event</Button>;
+function CourierTransportExample({
+  createTestEvent, subscribe, unsubscribe,
+  channel:_channel, event: _event,
+}) {
+  const [subScribeChannel, setSubscribeChannel] = useState(_channel);
+  const [subscribeEvent, setSubscribeEvent] = useState(_event);
+  const [unsubscribeChannel, setUnsubscribeChannel] = useState(_channel);
+  const [unsubscribeEvent, setUnsubscribeEvent] = useState(_event);
+  return (<div>
+    <Button onClick={createTestEvent}>Send Event</Button>
+    <div style={{ display:"flex" }}>
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        marginTop: 10,
+        maxWidth: 200,
+        marginRight: 20,
+      }}>
+        <Label>Channel</Label>
+        <Input placeholder="Channel" value={subScribeChannel} onChange={(e) => setSubscribeChannel(e.target.value)}/>
+        <Label>Event</Label>
+        <Input placeholder="Event" value={subscribeEvent} onChange={(e) => setSubscribeEvent(e.target.value)}/>
+        <Button onClick={() => subscribe(subScribeChannel, subscribeEvent)}>Subscribe</Button>
+      </div>
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        marginTop: 10,
+        maxWidth: 200,
+      }}>
+        <Label>Channel</Label>
+        <Input placeholder="Channel" value={unsubscribeChannel} onChange={(e) => setUnsubscribeChannel(e.target.value)}/>
+        <Label>Event</Label>
+        <Input placeholder="Event" value={unsubscribeEvent} onChange={(e) => setUnsubscribeEvent(e.target.value)}/>
+        <Button onClick={() => unsubscribe(unsubscribeChannel, unsubscribeEvent)}>Unsubscribe</Button>
+      </div>
+    </div>
+  </div>);
 }
--- stories/styled.js
+++ stories/styled.js
@@ -12,3 +12,21 @@ export const Button = styled.button`
   font-family: Verdana;
   cursor: pointer;
 `;
+
+export const Input = styled.input`
+  border: none;
+  outline: none;
+  background-color: lightgray;
+  border-radius: 4px;
+  height: 45px;
+  width: calc(200px - 20px);
+  margin: 10px 0;
+  padding: 0 10px;
+  font-size: 14px;
+`;
+
+export const Label = styled.div`
+  font-size: 14px;
+  color: black;
+  font-family: Verdana;
+`;
