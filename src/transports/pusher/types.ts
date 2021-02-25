01222c8b41086e8ed40a8caccf913dd59dea3f01
RoyalSix <jay.a.scottjr@biola.edu>
2021-02-25 11:12:30 -0800

    created github action

--- src/transports/pusher/types.ts
+++
@@ -1,9 +0,0 @@
-import Pusher, { Options } from "pusher-js";
-
-export interface ITransportOptions {
-  appKey?: string;
-  channel?: string;
-  event?: string;
-  instance?: Pusher;
-  options?: Options;
-}
