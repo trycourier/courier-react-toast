6962927ee20f89c51818003a650aa466d4a9fe31
riley napier <riley@trycourer.com>
2021-03-04 15:33:19 -0800

    config

--- stories/with-no-timer.stories.js
+++ stories/with-no-timer.stories.js
@@ -11,30 +11,9 @@ export default {
   },
 };
 
-export function Default({ bodyText }) {
-  return (
-    <ToastProvider clientKey="client-key">
-      <DefaultComponent body={bodyText} />
-    </ToastProvider>
-  );
-}
-
 function DefaultComponent({ body }) {
   const [toast] = useToast();
   return <Button onClick={() => toast(body)}>Show Toast</Button>;
-}
-
-export function NoIcon({ bodyText }) {
-  return (
-    <ToastProvider
-      clientKey="client-key"
-      config={{
-        icon: false,
-      }}
-    >
-      <DefaultComponent body={bodyText} />
-    </ToastProvider>
-  );
 }
 
 export function NoTimer({ bodyText }) {
